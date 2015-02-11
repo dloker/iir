1704559494d01fd8561a85f6bbe43e648e03fd0f
David Loker <dloker@gmail.com>
2015-02-11 19:30:42 +0000

    Fixing vocab issues for HDP with hashtags and some stop words

--- lda/vocabulary.py
+++ lda/vocabulary.py
@@ -18,18 +18,20 @@ def load_file(filename):
     corpus = []
     f = open(filename, 'r')
     for line in f:
-        doc = re.findall(r'\w+(?:\'\w+)?',line)
+        doc = re.findall(r'\S+',line)
         if len(doc)>0:
             corpus.append(doc)
     f.close()
     return corpus
 
 #stopwords_list = nltk.corpus.stopwords.words('english')
-stopwords_list = "a,s,able,about,above,according,accordingly,across,actually,after,afterwards,again,against,ain,t,all,allow,allows,almost,alone,along,already,also,although,always,am,among,amongst,an,and,another,any,anybody,anyhow,anyone,anything,anyway,anyways,anywhere,apart,appear,appreciate,appropriate,are,aren,t,around,as,aside,ask,asking,associated,at,available,away,awfully,be,became,because,become,becomes,becoming,been,before,beforehand,behind,being,believe,below,beside,besides,best,better,between,beyond,both,brief,but,by,c,mon,c,s,came,can,can,t,cannot,cant,cause,causes,certain,certainly,changes,clearly,co,com,come,comes,concerning,consequently,consider,considering,contain,containing,contains,corresponding,could,couldn,t,course,currently,definitely,described,despite,did,didn,t,different,do,does,doesn,t,doing,don,t,done,down,downwards,during,each,edu,eg,eight,either,else,elsewhere,enough,entirely,especially,et,etc,even,ever,every,everybody,everyone,everything,everywhere,ex,exactly,example,except,far,few,fifth,first,five,followed,following,follows,for,former,formerly,forth,four,from,further,furthermore,get,gets,getting,given,gives,go,goes,going,gone,got,gotten,greetings,had,hadn,t,happens,hardly,has,hasn,t,have,haven,t,having,he,he,s,hello,help,hence,her,here,here,s,hereafter,hereby,herein,hereupon,hers,herself,hi,him,himself,his,hither,hopefully,how,howbeit,however,i,d,i,ll,i,m,i,ve,ie,if,ignored,immediate,in,inasmuch,inc,indeed,indicate,indicated,indicates,inner,insofar,instead,into,inward,is,isn,t,it,it,d,it,ll,it,s,its,itself,just,keep,keeps,kept,know,knows,known,last,lately,later,latter,latterly,least,less,lest,let,let,s,like,liked,likely,little,look,looking,looks,ltd,mainly,many,may,maybe,me,mean,meanwhile,merely,might,more,moreover,most,mostly,much,must,my,myself,name,namely,nd,near,nearly,necessary,need,needs,neither,never,nevertheless,new,next,nine,no,nobody,non,none,noone,nor,normally,not,nothing,novel,now,nowhere,obviously,of,off,often,oh,ok,okay,old,on,once,one,ones,only,onto,or,other,others,otherwise,ought,our,ours,ourselves,out,outside,over,overall,own,particular,particularly,per,perhaps,placed,please,plus,possible,presumably,probably,provides,que,quite,qv,rather,rd,re,really,reasonably,regarding,regardless,regards,relatively,respectively,right,said,same,saw,say,saying,says,second,secondly,see,seeing,seem,seemed,seeming,seems,seen,self,selves,sensible,sent,serious,seriously,seven,several,shall,she,should,shouldn,t,since,six,so,some,somebody,somehow,someone,something,sometime,sometimes,somewhat,somewhere,soon,sorry,specified,specify,specifying,still,sub,such,sup,sure,t,s,take,taken,tell,tends,th,than,thank,thanks,thanx,that,that,s,thats,the,their,theirs,them,themselves,then,thence,there,there,s,thereafter,thereby,therefore,therein,theres,thereupon,these,they,they,d,they,ll,they,re,they,ve,think,third,this,thorough,thoroughly,those,though,three,through,throughout,thru,thus,to,together,too,took,toward,towards,tried,tries,truly,try,trying,twice,two,un,under,unfortunately,unless,unlikely,until,unto,up,upon,us,use,used,useful,uses,using,usually,value,various,very,via,viz,vs,want,wants,was,wasn,t,way,we,we,d,we,ll,we,re,we,ve,welcome,well,went,were,weren,t,what,what,s,whatever,when,whence,whenever,where,where,s,whereafter,whereas,whereby,wherein,whereupon,wherever,whether,which,while,whither,who,who,s,whoever,whole,whom,whose,why,will,willing,wish,with,within,without,won,t,wonder,would,would,wouldn,t,yes,yet,you,you,d,you,ll,you,re,you,ve,your,yours,yourself,yourselves,zero".split(',')
+stopwords_list = "a,s,able,about,above,according,accordingly,across,actually,after,afterwards,again,against,ain,t,ain't,all,allow,allows,almost,alone,along,already,also,although,always,am,among,amongst,an,and,another,any,anybody,anyhow,anyone,anything,anyway,anyways,anywhere,apart,appear,appreciate,appropriate,are,aren,t,aren't,around,as,aside,ask,asking,associated,at,available,away,awfully,be,became,because,become,becomes,becoming,been,before,beforehand,behind,being,believe,below,beside,besides,best,better,between,beyond,bio,both,brief,but,by,c,mon,c'mon,c,s,came,can,can,t,can't,cannot,cant,cause,causes,certain,certainly,changes,clearly,co,com,come,comes,comment,comments,concerning,consequently,consider,considering,contain,containing,contains,corresponding,could,couldn,t,couldn't,course,currently,day,definitely,described,despite,did,didn,t,didn't,different,do,does,doesn,t,doesn't,doing,don,t,don't,done,down,downwards,during,each,edu,eg,eight,either,else,elsewhere,enough,entirely,especially,et,etc,even,ever,every,everybody,everyone,everything,everywhere,ex,exactly,example,except,far,few,fifth,first,five,followed,following,follows,for,former,formerly,forth,four,from,further,furthermore,get,gets,getting,given,gives,go,goes,going,gone,good,goodtimes,got,gotten,great,greetings,had,hadn,t,hadn't,happens,happy,hardly,has,hashtag,hasn,t,hasn't,have,haven,t,haven't,having,he,he,s,he's,hello,help,hence,her,here,here,s,here's,hereafter,hereby,herein,hereupon,hers,herself,hi,him,himself,his,hither,hopefully,how,howbeit,however,i,d,i'd,i,ll,i'll,i,m,i'm,i,ve,i've,ie,if,ignored,immediate,in,inasmuch,inc,indeed,indicate,indicated,indicates,inner,insofar,instead,into,inward,is,isn,t,isn't,it,it,d,it'd,it,ll,it'll,it,s,it's,its,itself,just,keep,keeps,kept,know,knows,known,last,lately,later,latter,latterly,least,less,lest,let,let,s,let's,like,liked,likes,likely,link,little,look,looking,looks,ltd,mainly,many,may,maybe,me,mean,meanwhile,merely,might,more,moreover,most,mostly,much,must,my,myself,name,namely,nd,near,nearly,necessary,need,needs,neither,never,nevertheless,new,next,nine,no,nobody,non,none,noone,nor,normally,not,nothing,novel,now,nowhere,obviously,of,off,often,oh,ok,okay,old,on,once,one,ones,online,only,onto,or,other,others,otherwise,ought,our,ours,ourselves,out,outside,over,overall,own,particular,particularly,per,perhaps,placed,please,plus,possible,presumably,probably,provides,que,quite,qv,rather,rd,re,really,reasonably,regarding,regardless,regards,relatively,respectively,right,said,same,saw,say,saying,says,second,secondly,see,seeing,seem,seemed,seeming,seems,seen,self,selves,sensible,sent,serious,seriously,seven,several,shall,she,should,shouldn,t,shouldn't,since,six,so,some,somebody,somehow,someone,something,sometime,sometimes,somewhat,somewhere,soon,sorry,specified,specify,specifying,still,sub,such,sup,sure,t,s,take,taken,tag,tell,tends,th,than,thank,thanks,thanx,that,that,s,that's,thats,the,their,theirs,them,themselves,then,thence,there,there,s,there's,thereafter,thereby,therefore,therein,theres,thereupon,these,they,they,d,they'd,they,ll,they'll,they,re,they're,they,ve,they've,think,third,this,thorough,thoroughly,those,though,three,through,throughout,thru,thus,time,to,today,together,too,took,toward,towards,tried,tries,truly,try,trying,twice,two,un,under,unfortunately,unless,unlikely,until,unto,up,upon,us,use,used,useful,uses,using,usually,value,various,very,via,viz,vs,want,wants,was,wasn,t,wasn't,way,we,we,d,we'd,we,ll,we'll,we,re,we're,we,ve,we've,welcome,well,went,were,weren,t,weren't,what,what,s,what's,whatever,when,whence,whenever,where,where,s,where's,whereafter,whereas,whereby,wherein,whereupon,wherever,whether,which,while,whither,who,who,s,who's,whoever,whole,whom,whose,why,will,willing,wish,with,within,without,won,t,won't,wonder,would,would,wouldn,t,wouldn't,yes,yet,you,you,d,you'd,you,ll,you'll,you,re,you're,you,ve,you've,your,yours,yourself,yourselves,zero".split(',')
 recover_list = {"wa":"was", "ha":"has"}
 wl = nltk.WordNetLemmatizer()
 
 def is_stopword(w):
+    if w[0] == '#':
+        w = w[1:]
     return w in stopwords_list
 def lemmatize(w0):
     w = wl.lemmatize(w0.lower())
@@ -45,8 +47,11 @@ class Vocabulary:
         self.excluds_stopwords = excluds_stopwords
 
     def term_to_id(self, term0):
-        term = lemmatize(term0)
-        if not re.match(r'[a-z]+$', term): return None
+        #term = lemmatize(term0)
+        term = term0.lower()
+        #if not re.match(r'^(#){0,1}[a-z]+(_[a-z]+)*$', term): return None
+        if not re.match(r'^(#){0,1}[^#]*$', term): return None
+        if len(term) <= 2 or re.match(r'^[^a-z0-9]+$', term): return None
         if self.excluds_stopwords and is_stopword(term): return None
         if term not in self.vocas_id:
             voca_id = len(self.vocas)
@@ -62,7 +67,7 @@ class Vocabulary:
         list = []
         words = dict()
         for term in doc:
-            id = self.term_to_id(term)
+            id = self.term_to_id(term.strip())
             if id != None:
                 list.append(id)
                 if not words.has_key(id):
